b992f9c95ec2c1f128245736484d1a2504d6b199
lynx-fx <anupbhujel07@gmail.com>
2025-06-18 12:50:02 +0545

    Added identifier middleware

--- server/middleware/identifier.js
+++ server/middleware/identifier.js
@@ -1,3 +1,31 @@
-exports.identifier = () => {
-    
-}
+require("dotenv").config("../.env");
+const jwt = require("jsonwebtoken");
+const tokenExtractor = require("../utils/tokenExtractor");
+
+exports.identifier = (req, res, next) => {
+  const token = tokenExtractor(req);
+  if (!token) {
+    return res
+      .status()
+      .json({
+        success: false,
+        message: "No token provided for identification.",
+      });
+  }
+  try {
+    const decode = jwt.verify(token, process.env.TOKEN_SECRET);
+    if (decode) {
+      req.user = jwtVerified;
+      next();
+    } else {
+      return res
+        .status(401)
+        .json({ success: false, message: "Idenfitication failed." });
+    }
+  } catch (err) {
+    console.console.log(err);
+    return res
+      .status(500)
+      .json({ success: false, message: "Identification error." });
+  }
+};
