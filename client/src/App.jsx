460c7eef482dc5e14d98fbf0df3eeddd3cd2701c
lynx-fx <anupbhujel07@gmail.com>
2025-06-26 10:42:03 +0545

    Added authorization and redirection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { toast, Toaster } from "sonner";
 import Landing from "./component/landing.jsx";
 import WarrantyDashboard from "./component/dashboard.jsx";
@@ -12,13 +12,36 @@ import ChangePassword from "./component/changePassword.jsx";
 import NotFound from "./component/notFound.jsx";
 
 function App() {
+  const getCookie = (name) => {
+    const match = document.cookie.match(
+      new RegExp("(^| )" + name + "=([^;]+)")
+    );
+    return match ? match[2] : null;
+  };
+
+  const isLoggedIn = !!getCookie("auth");
+
   return (
     <>
       <Toaster richColors />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<WarrantyDashboard />} />
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? <Navigate to="/dashboard" replace /> : <Landing />
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              !isLoggedIn ? (
+                <Navigate to="/login" replace />
+              ) : (
+                <WarrantyDashboard />
+              )
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
