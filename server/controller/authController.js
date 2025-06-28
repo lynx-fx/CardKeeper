5fb461fb8f70c5fb32254978941844caff106375
lynx-fx <anupbhujel07@gmail.com>
2025-06-28 09:59:28 +0545

    Deadass env

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,4 +1,4 @@
-require("dotenv").config("../.env");
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 // middleware
--- server/controller/cardController.js
+++ server/controller/cardController.js
@@ -1,4 +1,4 @@
-require("dotenv").config("../.env");
+require("dotenv").config();
 const express = require("express");
 const { tokenExtractor } = require("../utils/tokenExtractor");
 const jwt = require("jsonwebtoken");
