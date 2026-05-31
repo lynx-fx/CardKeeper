9eb4dc9da69b44533027afe3907b8764103ac49d
lynx-fx <anupbhujel07@gmail.com>
2026-05-31 20:19:46 +0545

    ffeat: updated cards controller

--- server/src/card/card.controller.ts
+++ server/src/card/card.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Request, Param, Delete, UseGuards } from '@nestjs/common';
 import { CardService } from './card.service';
-import { CreateCardDto, CreateCardResponseDto } from './dto/card.dto';
+import { CreateCardDto, CreateCardResponseDto, DeleteCardResponseDto, GetCardResponseDto, GetCardsResponseDto, UpdateCardResponseDto } from './dto/card.dto';
 import { UpdateCardDto } from './dto/card.dto';
 import { JwtGuard } from '../../guard/jwtVerifyGuard';
 import { ApiOkResponse } from '@nestjs/swagger';
@@ -18,24 +18,36 @@ export class CardController {
     return this.cardService.create(+req.user.user_id, createCardDto);
   }
 
+  @ApiOkResponse({
+    type: GetCardsResponseDto
+  })
   @Get()
   @UseGuards(JwtGuard)
   findAll(@Request() req) {
     return this.cardService.findAll(+req.user.user_id);
   }
 
+  @ApiOkResponse({
+    type: GetCardResponseDto
+  })
   @Get(':id')
   @UseGuards(JwtGuard)
   findOne(@Request() req, @Param('id') id: string) {
     return this.cardService.findOne(+req.user.user_id, +id);
   }
 
+  @ApiOkResponse({
+    type: UpdateCardResponseDto
+  })
   @Patch(':id')
   @UseGuards(JwtGuard)
   update(@Request() req,@Param('id') id: string, @Body() updateCardDto: UpdateCardDto) {
     return this.cardService.update(+req.user.user_id, +id, updateCardDto);
   }
 
+  @ApiOkResponse({
+    type: DeleteCardResponseDto
+  })
   @Delete(':id')
   @UseGuards(JwtGuard)
   remove(@Request() req,@Param('id') id: string) {
--- server/src/card/card.service.ts
+++ server/src/card/card.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CardDto, CreateCardDto, CreateCardResponseDto, GetCardResponseDto, GetCardsResponseDto, UpdateCardDto } from './dto/card.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { AUTH_RESPONSE } from '../auth/constants/auth-messages';
@@ -13,7 +13,6 @@ export class CardService {
 
   async create(user: number, createCardDto: CreateCardDto): Promise<CreateCardResponseDto> {
     const existingUser: User = await this.findExistingUser(user);
-    if (!existingUser) this.userNotFound();
 
     await this.prisma.card.create({
       data: {
@@ -26,46 +25,78 @@ export class CardService {
   }
 
   async findAll(user: number): Promise<GetCardsResponseDto> {
-    const existingUser : User = await this.findExistingUser(user);
+    const existingUser: User = await this.findExistingUser(user);
 
-    const cards = await this.prisma.card.findMany({where: {user_id: existingUser.user_id}})
+    const cards = await this.prisma.card.findMany({ where: { user_id: existingUser.user_id } })
 
-    if (cards.length === 0){
-      throw new NotFoundException({success: false, message: "No cards available"})
+    if (cards.length === 0) {
+      throw new NotFoundException({ success: false, message: "No cards available" })
     }
 
     const cardsDto = plainToInstance(CardDto, cards);
 
-    return {success: true, cards: cardsDto}
+    return { success: true, cards: cardsDto }
   }
 
-  async findOne(user: number, id: number) {
-    return { success: true, message: "" };
+  async findOne(user: number, card_id: number): Promise<GetCardResponseDto> {
+    const exisingUser: User = await this.findExistingUser(user);
+
+    const card = await this.prisma.card.findUnique({ where: { card_id } });
+
+    if (!card) {
+      throw new NotFoundException({ success: false, message: "Card details not fonud" })
+    }
+
+    const cardDto = plainToInstance(CardDto, card);
+
+    return { success: true, card: cardDto }
   }
 
-  async update(user: number, id: number, updateCardDto: UpdateCardDto) {
-    return { success: true, message: "" };
-  }
+  async update(user: number, card_id: number, updateCardDto: UpdateCardDto) {
+    const existingUser: User = await this.findExistingUser(user);
 
-  async remove(user: number, id: number) {
-    return { success: true, message: "" };
-  }
-
-  private userNotFound(): never {
-    throw new NotFoundException({
-      success: false,
-      message: AUTH_RESPONSE.USER_NOT_FOUND
+    await this.prisma.card.update({
+      where: { card_id },
+      data: {
+        ...updateCardDto
+      }
     })
+
+    return { success: true, message: `${updateCardDto.productName} udpated successfully` }
   }
+
+  async remove(user: number, card_id: number) {
+    const exisitngUser: User = await this.findExistingUser(user);
+
+    const card = await this.prisma.card.findFirst({ where: { card_id, user_id: exisitngUser.user_id } });
+    if (!card) throw new NotFoundException({ success: false, message: "Card not found" });
+
+    // delete the images first and then card
+    await this.prisma.$transaction([
+      this.prisma.image.deleteMany({
+        where: { card_id }
+      }),
+      this.prisma.card.delete({
+        where: { card_id }
+      })
+    ])
+
+    return { success: true, message: "Card successfully deleted" };
+  }
+
 
   private async findExistingUser(user_id?: number, email?: string): Promise<User> {
     const user = user_id
-    ? await this.prisma.user.findUnique({ where: { user_id } })
-    : email
-    ? await this.prisma.user.findUnique({ where: { email } })
-    : null;
+      ? await this.prisma.user.findUnique({ where: { user_id } })
+      : email
+        ? await this.prisma.user.findUnique({ where: { email } })
+        : null;
 
-    if (!user) this.userNotFound();
+    if (!user) throw new NotFoundException({
+      success: false,
+      message: AUTH_RESPONSE.USER_NOT_FOUND
+    });
+
     return user;
   }
 
